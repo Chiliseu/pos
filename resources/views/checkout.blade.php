f3e7637661abfe6eb106819e02834f1170c2c46e
Yuan Evangelista <yuanexequielevangelista03@gmail.com>
2025-01-09 14:47:24 +0800

    utot4

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -325,7 +325,7 @@ $total = $subtotal; // Discount temporarily set to 0
                     </div>
                     {{-- onsubmit="return confirmPayment()" --}}
                     <div class="paymentButtonContainer">
-                        <form action="#" method="POST">
+                        <form action="#" method="POST" onsubmit="return confirmPayment()">
                             @csrf
                                 <!-- PHP HERE FOR  PAYMENT LOG? -->
                             <?php foreach ($products as $product): ?>
