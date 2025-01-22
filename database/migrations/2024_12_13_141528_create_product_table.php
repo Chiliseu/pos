5c6bc440ab86dc72175c12cf44cbd0642dd9f7e1
Rj-Aquino <richardjasonmaquino@iskolarngbayan.pup.edu.ph>
2025-01-22 11:20:29 +0800

    Added new Column for UID for Product and added Seeder

--- database/migrations/2024_12_13_141528_create_product_table.php
+++ database/migrations/2024_12_13_141528_create_product_table.php
@@ -4,33 +4,34 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
     public function up()
     {
-        // Check if the 'product' table exists before creating it
-        if (!Schema::hasTable('product')) {
-            Schema::create('product', function (Blueprint $table) {
-                $table->id('ProductID'); // Primary key
-                
-                $table->unsignedBigInteger('CategoryID');
-                $table->foreign('CategoryID')->references('CategoryID')->on('category')->onDelete('cascade');
+        Schema::create('product', function (Blueprint $table) {
+            $table->id('ProductID'); // Primary key
+            
+            $table->unsignedBigInteger('CategoryID');
+            $table->foreign('CategoryID')->references('CategoryID')->on('category')->onDelete('cascade');
 
-                $table->string('Name', 100);
-                $table->float('Price');
-                $table->timestamps();
-            });
-        }
+            $table->string('Name', 100);
+            $table->float('Price');
+            $table->string('UniqueIdentifier', 50)->unique(); // Add the unique identifier column
+            $table->timestamps();
+        });
     }
 
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
     public function down()
     {
-        // Drop the 'product' table if it exists
-        if (Schema::hasTable('product')) {
-            Schema::dropIfExists('product');
-        }
+        Schema::dropIfExists('product');
     }
 };
