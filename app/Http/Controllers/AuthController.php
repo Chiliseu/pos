ce941356d62a9ba17df3c7995bcceb523b4f3552
Rj-Aquino <richardjasonmaquino@iskolarngbayan.pup.edu.ph>
2025-01-27 23:28:58 +0800

    Routes test 1

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -103,6 +103,6 @@ class AuthController extends Controller
         $request->session()->invalidate();
         $request->session()->regenerateToken();
 
-        return redirect('/login'); // Redirect to login page
+        return redirect('/'); // Redirect to login page
     }
 }
